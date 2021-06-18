20bb7c52b11ae8d8a9f6577e7180fc402bff38fc
Cindy.a <cindy.aloui@gmail.com>
2021-06-18 09:46:02 +0200

    Add files via upload

---
+++ app/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+import os
+
+SECRET_KEY = os.urandom(32)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = SECRET_KEY
+
+class GreetUserForm(FlaskForm):
+    username = StringField(label=('Enter Your Name:'))
+    submit = SubmitField(label=('Submit'))
+
+@app.route('/', methods=('GET', 'POST'))
+def index():
+    form = GreetUserForm()
+    if form.validate_on_submit():
+        return f'''<h1> Welcome {form.username.data} </h1>'''
+    return render_template('index.html', form=form)
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+#
+# @app.route("/")
+# def home_view():
+#     return "<h1>Welcome to Geeks for Geeks</h1>"
